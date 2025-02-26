b0adc380c9ac4f1e4d195310be84e6ea49340c1a
Tom <tmkirsch11@gmail.com>
2025-02-26 19:29:24 +0000

    Changed it so keys now control robot

--- env.py
+++ env.py
@@ -2,39 +2,57 @@ import math
 import pygame
 
 class buildEnvironment:
-    def __init__(self,MapDimensions):
+    def __init__(self, MapDimensions):
         pygame.init()
-        self.pointCloud=[]
-        self.externalMap=pygame.image.load('map1.png')
+        self.pointCloud = []
+        self.externalMap = pygame.image.load('map1.png')
         self.maph, self.mapw = MapDimensions
         self.MapWindowName = 'Mapping - Tom Kirsch'
         pygame.display.set_caption(self.MapWindowName)
         self.map = pygame.display.set_mode((self.mapw, self.maph))
-        self.map.blit(self.externalMap, (0,0))
-        #Colours
-        self.black = (0,0,0)
+        self.map.blit(self.externalMap, (0, 0))
+        
+        # Colours
+        self.black = (0, 0, 0)
         self.grey = (70, 70, 70)
-        self.blue = (0,0,255)
-        self.green = (0,255,0)
-        self.red = (255,0,0)
+        self.blue = (0, 0, 255)
+        self.green = (0, 255, 0)
+        self.red = (255, 0, 0)
         self.white = (255, 255, 255)
 
+        # Robot position (default center)
+        self.robot_position = (self.mapw // 2, self.maph // 2)
 
-    def AD2pos(self,distance,angle,robotPosition):
-        x = distance *math.cos(angle)+robotPosition[0]
-        y = -distance *math.sin(angle)+robotPosition[1]
+    def AD2pos(self, distance, angle, robotPosition):
+        x = distance * math.cos(angle) + robotPosition[0]
+        y = -distance * math.sin(angle) + robotPosition[1]
         return (int(x), int(y))
 
-
-    def dataStorage(self,data):
+    def dataStorage(self, data):
         print(len(self.pointCloud))
         for element in data:
-            point=self.AD2pos(element[0], element[1], element[2])
+            point = self.AD2pos(element[0], element[1], element[2])
             if point not in self.pointCloud:
                 self.pointCloud.append(point)
 
-
     def show_sensorData(self):
-        self.infomap=self.map.copy()
+        self.infomap = self.map.copy()
+        
+        # Draw the scanned points
         for point in self.pointCloud:
-            self.infomap.set_at((int(point[0]), int(point[1])), (255,0,0))
+            self.infomap.set_at((int(point[0]), int(point[1])), self.red)
+        
+        self.infomap.set_at((int(self.robot_position[0]), int(self.robot_position[1])), self.green)
+
+
+    def update_robot_position(self, new_position):
+        """ Updates the robot's position while keeping sensor data. """
+        # Restore the previous robot position to match the background color
+        self.infomap.set_at((int(self.robot_position[0]), int(self.robot_position[1])), self.black)
+
+        # Update the robot's position
+        self.robot_position = new_position
+
+        # Draw new robot position
+        self.infomap.set_at((int(self.robot_position[0]), int(self.robot_position[1])), self.green)
+
--- main.py
+++ main.py
@@ -2,32 +2,44 @@ import env, sensors
 import pygame
 import math
 
+# Initialize environment
 environment = env.buildEnvironment((600, 1200))
 environment.originalMap = environment.map.copy()
 laser = sensors.LaserSensor(200, environment.originalMap, uncertainty=(0.5, 0.01))
-environment.map.fill((0,0,0))
+environment.map.fill((0, 0, 0))
 environment.infomap = environment.map.copy()
 
+# Start position at the center of the screen
+position = [environment.map.get_width() // 2, environment.map.get_height() // 2]
+speed = 5  # Movement speed
 
 running = True
 
 while running:
-    sensorOn = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if pygame.mouse.get_focused():
-            sensorOn = True
-        elif not pygame.mouse.get_focused():
-            sensorOn = False
 
-    if sensorOn:
-        position=pygame.mouse.get_pos()
-        laser.position = position
-        sensor_data = laser.sense_obstacles()
-        if sensor_data != -1:
-            environment.dataStorage(sensor_data)
-            environment.show_sensorData()
-    
-    environment.map.blit(environment.infomap,(0,0))
+    # Get key states
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        position[0] -= speed  # Move left
+    if keys[pygame.K_RIGHT]:
+        position[0] += speed  # Move right
+    if keys[pygame.K_UP]:
+        position[1] -= speed  # Move up
+    if keys[pygame.K_DOWN]:
+        position[1] += speed  # Move down
+
+
+    # Update laser position
+    laser.position = tuple(position)
+    sensor_data = laser.sense_obstacles()
+    if sensor_data != -1:
+        environment.dataStorage(sensor_data)
+        environment.show_sensorData()
+        environment.update_robot_position(tuple(position))
+
+    # Update display
+    environment.map.blit(environment.infomap, (0, 0))
     pygame.display.update()
